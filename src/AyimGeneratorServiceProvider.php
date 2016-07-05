c1eb6cf9aa7a9dbf068cd6786ca8e309455ed5e2
Darth Vader <ayimdomnic@outlook.com>
2016-07-06 00:54:51 +0300

    Commands then testing

--- src/AyimGeneratorServiceProvider.php
+++ src/AyimGeneratorServiceProvider.php
@@ -4,6 +4,23 @@ namespace Ayim\Generator;
     
 use Illuminate\Support\ServiceProvider;
 
+use Ayim\Generator\Commands\API\APIControllerGeneratorCommand;
+use Ayim\Generator\Commands\API\APIGeneratorCommand;
+use Ayim\Generator\Commands\API\APIRequestsGeneratorCommand;
+use Ayim\Generator\Commands\API\TestsGeneratorCommand;
+use Ayim\Generator\Commands\APIScaffoldGeneratorCommand;
+use Ayim\Generator\Commands\Common\MigrationGeneratorCommand;
+use Ayim\Generator\Commands\Common\ModelGeneratorCommand;
+use Ayim\Generator\Commands\Common\RepositoryGeneratorCommand;
+use Ayim\Generator\Commands\Publish\GeneratorPublishCommand;
+use Ayim\Generator\Commands\Publish\LayoutPublishCommand;
+use Ayim\Generator\Commands\Publish\PublishTemplateCommand;
+use Ayim\Generator\Commands\RollbackGeneratorCommand;
+use Ayim\Generator\Commands\Scaffold\ControllerGeneratorCommand;
+use Ayim\Generator\Commands\Scaffold\RequestsGeneratorCommand;
+use Ayim\Generator\Commands\Scaffold\ScaffoldGeneratorCommand;
+use Ayim\Generator\Commands\Scaffold\ViewsGeneratorCommand;
+
 
 class AyimGeneratorServiceProvider extends ServiceProvider
 {
@@ -21,12 +38,96 @@ class AyimGeneratorServiceProvider extends ServiceProvider
     
     public function register()
     {
-        //register all commands here
-        //sudo code
-        //this->app->singleton(blah!blah!)
+        $this->app->singleton('ayim.publish', function ($app) {
+            return new GeneratorPublishCommand();
+        });
         
-        //this->commands([
-        //list them here
-       // ])
+        $this->app->singleton('ayim.api', function ($app) {
+            return new APIGeneratorCommand();
+        });
+        
+        $this->app->singleton('ayim.scaffold', function ($app) {
+            return new ScaffoldGeneratorCommand();
+        });
+        
+        $this->app->singleton('ayim.publish.layout', function ($app) {
+            return new LayoutPublishCommand();
+        });
+        
+        $this->app->singleton('ayim.publish.templates', function ($app) {
+            return new PublishTemplateCommand();
+        });
+        $this->app->singleton('ayim.api_scaffold', function ($app) {
+            return new APIScaffoldGeneratorCommand();
+        });
+        
+        $this->app->singleton('ayim.migration', function ($app) {
+            return new MigrationGeneratorCommand();
+        });
+        
+        $this->app->singleton('ayim.model', function ($app) {
+            return new ModelGeneratorCommand();
+        });
+        
+        $this->app->singleton('ayim.repository', function ($app) {
+            return new RepositoryGeneratorCommand();
+        });
+        
+        $this->app->singleton('ayim.api.controller', function ($app) {
+            return new APIControllerGeneratorCommand();
+        });
+        
+        $this->app->singleton('ayim.api.requests', function ($app) {
+            return new APIRequestsGeneratorCommand();
+        });
+        
+        $this->app->singleton('ayim.api.tests', function ($app) {
+            return new TestsGeneratorCommand();
+        });
+        
+        $this->app->singleton('ayim.scaffold.controller', function ($app) {
+            return new ControllerGeneratorCommand();
+        });
+        
+        $this->app->singleton('ayim.scaffold.requests', function ($app) {
+            return new RequestsGeneratorCommand();
+        });
+        
+        $this->app->singleton('ayim.scaffold.views', function ($app) {
+            return new ViewsGeneratorCommand();
+        });
+        
+        $this->app->singleton('ayim.rollback', function ($app) {
+            return new RollbackGeneratorCommand();
+        });
+        
+//        $this->app->singleton('ayim.vuejs', function ($app) {
+//            return new VueJsGeneratorCommand();
+//        });
+        
+//        $this->app->singleton('ayim.publish.vuejs', function ($app) {
+//            return new VueJsLayoutPublishCommand();
+//        }); 
+        
+        $this->commands([
+            'ayim.publish',
+            'ayim.api',
+            'ayim.scaffold',
+            'ayim.api_scaffold',
+            'ayim.publish.layout',
+            'ayim.publish.templates',
+            'ayim.migration',
+            'ayim.model',
+            'ayim.repository',
+            'ayim.api.controller',
+            'ayim.api.requests',
+            'ayim.api.tests',
+            'ayim.scaffold.controller',
+            'ayim.scaffold.requests',
+            'ayim.scaffold.views',
+            'ayim.rollback'
+//            'ayim.vuejs',
+//            'ayim.publish.vuejs'
+        ]);
     }
 }
